9079b6cabdaf694d68f53ed322c3d20137e1ea69
Raphael Della Vecchia <ddellave@cs.cmu.edu>
2024-11-15 10:21:19 -0500

    Moved error handling macros to a separate file error.h

---
+++ project-2_15-441/inc/error.h
@@ -0,0 +1,19 @@
+#ifndef ERROR_H
+#define ERROR_H
+
+#define CHK(__va_args__)\
+{\
+  if(!(__va_args__)) {\
+    return EXIT_FAILURE; }\
+}
+
+#define CHK_MSG(MSG, __va_args__)\
+{\
+  if(!(__va_args__)) {\
+    perror(MSG);\
+    return EXIT_FAILURE; \
+  }\
+}
+
+
+#endif
