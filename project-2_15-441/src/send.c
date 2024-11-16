9cf84c20fc6cbb5eb8044ef31bdf5375cc325e23
kvanlaan <kdvanlaan@gmail.com>
2024-11-16 13:20:18 -0500

    fix(dup_ack_cnt): dup_ack_cnt

--- project-2_15-441/src/send.c
+++ project-2_15-441/src/send.c
@@ -110,7 +110,7 @@ cmu_tcp_header_t* chk_send_pkt(cmu_socket_t *sock) {
 
     gettimeofday(&now, NULL);
     sock->window.last_send = now.tv_sec;
-    sock->window.dup_ack_cnt = 0;
+    // sock->window.dup_ack_cnt = 0;
     return pkt;
   }
 
