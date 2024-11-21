318c25d79f7419715602bc59a2293f5f3e2785d3
kvanlaan <kdvanlaan@gmail.com>
2024-11-21 13:26:27 -0500

    re-add dupacck reset on timeout

--- project-2_15-441/src/send.c
+++ project-2_15-441/src/send.c
@@ -110,8 +110,8 @@ cmu_tcp_header_t *chk_send_pkt(cmu_socket_t *sock) {
 
     gettimeofday(&now, NULL);
     sock->window.last_send = now.tv_sec;
-
-    // sock->window.dup_ack_cnt = 0;
+    
+    sock->window.dup_ack_cnt = 0;
     return pkt;
   }
 
