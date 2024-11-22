388805b00e49afab3bc5aa48526ee24ced60c7cc
kvanlaan <kdvanlaan@gmail.com>
2024-11-21 21:23:37 -0500

    make sure that cwin and ssthresh never < MSS

--- project-2_15-441/src/cca.c
+++ project-2_15-441/src/cca.c
@@ -32,6 +32,8 @@ void cca_dup_ack_3(cmu_socket_t *sock) {
   } else {
       sock->window.cwin += MSS;
   }
+  sock->window.cwin = MAX(sock->window.cwin, MSS);
+  sock->ssthresh = MAX(sock->ssthresh, MSS);
   return;
 }
 
@@ -46,6 +48,8 @@ void cca_new_ack(cmu_socket_t *sock) {
   } else {  // congestion avoidance
     sock->window.cwin += (MSS * (MSS / sock->window.cwin));
   }
+  sock->window.cwin = MAX(sock->window.cwin, MSS);
+  sock->ssthresh = MAX(sock->ssthresh, MSS);
   return;
 }
 
@@ -85,5 +89,6 @@ void cca_enter_ss_from_timeout(cmu_socket_t *sock) {
   sock->is_fast_recovery = 0;
   sock->ssthresh = sock->window.cwin / 2;
   sock->window.cwin = MSS;
+  sock->ssthresh = MAX(sock->ssthresh, MSS);
   return;
 }
