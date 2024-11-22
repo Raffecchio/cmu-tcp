6b9e4e56a1ef2d6fd59ded91aa937e601d7fd173
kvanlaan <kdvanlaan@gmail.com>
2024-11-21 21:03:44 -0500

    cleanup

--- project-2_15-441/src/cca.c
+++ project-2_15-441/src/cca.c
@@ -50,6 +50,7 @@ void cca_new_ack(cmu_socket_t *sock) {
 }
 
 void fast_recovery(cmu_socket_t *sock) {
+  fill_send_win(sock);
   sock->is_fast_recovery = 1;
   cmu_tcp_header_t *pkt_send = get_win_pkt(sock, 0);
   // Note: 
@@ -73,8 +74,8 @@ void fast_recovery(cmu_socket_t *sock) {
     cca_enter_ss_from_timeout(sock);
     return;
   }
-  if ((fast_rec_ack_pkt != NULL) && is_valid_recv(sock, fast_rec_ack_pkt)) {
-    on_recv_pkt(sock, fast_rec_ack_pkt);
+  if ((fast_rec_ack_pkt != NULL) && is_valid_recv(sock, (const cmu_tcp_header_t *)fast_rec_ack_pkt)) {
+    on_recv_pkt(sock, (const cmu_tcp_header_t *) fast_rec_ack_pkt);
   }
   return;
 }
