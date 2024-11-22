9466d9f0dc9cfa6891b63327aefd1a901f515152
kvanlaan <kdvanlaan@gmail.com>
2024-11-22 00:43:22 -0500

    test no fast retrans

--- project-2_15-441/src/cca.c
+++ project-2_15-441/src/cca.c
@@ -29,7 +29,7 @@ void cca_dup_ack_3(cmu_socket_t *sock) {
       ssthresh = cwin * .5;
       sock->window.cwin = ssthresh + (3 * MSS);
       sock->is_fast_recovery = 1;
-      fast_recovery(sock); 
+      // fast_recovery(sock); 
   } else {
       sock->window.cwin += MSS;
   }
